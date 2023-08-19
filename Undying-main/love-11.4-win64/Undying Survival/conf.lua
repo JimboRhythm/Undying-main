8aee8d19923e89bc208cf38cf217da26b058ed61
JimboRhythm <130548698+JimboRhythm@users.noreply.github.com>
2023-08-20 06:01:22 +0800

    he
    yoooo

---
+++ Undying-main/love-11.4-win64/Undying Survival/conf.lua
@@ -0,0 +1,12 @@
+function love.conf(t)
+    t.title = "Undying Survival 1.0v"
+    t.version = "11.4"
+    t.window.width = 800
+    t.window.height = 600
+    t.window.resizable = true
+    t.window.minwidth = 400
+    t.window.minheight = 300
+    t.modules.audio = true
+    t.modules.keyboard = true
+    t.modules.event = true
+end
---
+++ Undying-main/readme.txt
@@ -0,0 +1,7 @@
+Hello bro, thanks for downloading this non effort game! This game is designed to be basic and bad and its also my first game. So you can hate it whatever you want.
+
+ill release the source code when the game reaches at 1.2
+
+
+
+so yeah enjoy the game lol
